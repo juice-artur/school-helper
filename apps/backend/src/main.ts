9ab23cdf00f4623284d2d4ff108cfb7ba778aa3e
juice-artur <asasin.lol10@gmail.com>
2024-04-22 00:01:59 +0300

    Added nesrjs/config

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen('3005');
-  //await app.listen('BACKEND_PORT');
+  const configService = app.get<ConfigService>(ConfigService);
+
+  await app.listen(configService.get('BACKEND_PORT') || 3000);
 }
 bootstrap();
