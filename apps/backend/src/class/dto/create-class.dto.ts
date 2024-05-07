70ec9723fcb60aaeaad4d1d973c459d28a588a35
juice-artur <asasin.lol10@gmail.com>
2024-05-07 22:49:53 +0300

    Fix swagger

--- apps/backend/src/class/dto/create-class.dto.ts
+++ apps/backend/src/class/dto/create-class.dto.ts
@@ -1,3 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateClassDto {
+  @ApiProperty({
+    example: '11',
+    description: 'Order',
+  })
+  @IsNotEmpty()
   order: string;
 }
