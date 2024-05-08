85e7a8106e1c897e8d0ac0220540f1337de91d82
juice-artur <asasin.lol10@gmail.com>
2024-05-08 23:59:40 +0300

    Fix roles

--- apps/backend/src/user/dto/response.dto.ts
+++ apps/backend/src/user/dto/response.dto.ts
@@ -1,4 +1,26 @@
+import { Role, UserRole } from '@prisma/client';
+import { Exclude, Expose, Transform, plainToInstance } from 'class-transformer';
+
+class RoleDTO {
+  @Exclude()
+  id: string;
+  role: Role;
+  @Exclude()
+  userId: string;
+}
 export class ResponseUserDto {
+  id: string;
   firstName: string;
   lastName: string;
+  email: string;
+  password?: string;
+  googleId?: string;
+  avatarKey?: string;
+  isActive: boolean;
+  schoolId?: string;
+  @Expose()
+  @Transform(({ value }) => {
+    return plainToInstance(RoleDTO, value);
+  })
+  userRoles: string[];
 }
