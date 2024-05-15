401dd82cfe7d90a891f4e33218228b5d742be1c6
juice-artur <asasin.lol10@gmail.com>
2024-05-15 23:33:01 +0300

    feat: implement `get students by class id`

--- apps/backend/src/student/student.controller.ts
+++ apps/backend/src/student/student.controller.ts
@@ -1,34 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { StudentService } from './student.service';
-import { CreateStudentDto } from './dto/create-student.dto';
-import { UpdateStudentDto } from './dto/update-student.dto';
+import { ApiOperation, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Role } from '@prisma/client';
+import { JwtGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/decorators/roles.decorator';
+import { ResponseStudentDto } from './dto/responce-student.dto';
 
+@ApiTags('Students endpoints')
 @Controller('student')
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
-  @Post()
-  create(@Body() createStudentDto: CreateStudentDto) {
-    return this.studentService.create(createStudentDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.studentService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.studentService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
-    return this.studentService.update(+id, updateStudentDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.studentService.remove(+id);
+  @Get('all-by-class/:id')
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(Role.TEACHER)
+  @ApiOperation({ summary: 'Create quiz' })
+  @ApiOkResponse({
+    type: ResponseStudentDto,
+  })
+  getAllBtyClassId(@Param('id') id: string) {
+    return this.studentService.findAllByClassId(id);
   }
 }
--- apps/backend/src/student/student.service.ts
+++ apps/backend/src/student/student.service.ts
@@ -1,26 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { CreateStudentDto } from './dto/create-student.dto';
-import { UpdateStudentDto } from './dto/update-student.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class StudentService {
-  create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
-  }
-
-  findAll() {
-    return `This action returns all student`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} student`;
-  }
-
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return `This action updates a #${id} student`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} student`;
+  constructor(private readonly prismaService: PrismaService) {}
+  findAllByClassId(id: string) {
+    return this.prismaService.student.findMany({
+      where: { classId: id },
+      include: { user: true },
+    });
   }
 }
