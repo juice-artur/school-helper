8f1d4cf44009185b2d83d43d11c4017db3a4aeb0
juice-artur <asasin.lol10@gmail.com>
2024-05-20 21:57:31 +0300

    feat: implement `getPendingInviteUser`

--- apps/backend/src/invite/invite.controller.ts
+++ apps/backend/src/invite/invite.controller.ts
@@ -13,6 +13,7 @@ import { JwtGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/decorators/roles.decorator';
 import { MailService } from 'src/mail/mail.service';
+import { ResponseStudentDto } from 'src/student/dto/responce-student.dto';
 
 @Controller('invite')
 @ApiTags('Invite endpoints')
@@ -51,5 +52,16 @@ export class InviteController {
   async acceptInviteStudent(@Param('id') id: string) {
     const invitation = await this.inviteService.acceptById(id);
     return invitation;
+  }
+
+  @Get('pending/:id')
+  @ApiOperation({ summary: 'Get all pending Invite by class id' })
+  @ApiOkResponse({
+    type: ResponseStudentDto,
+  })
+  @ApiParam({ name: 'id', type: 'string' })
+  async getPendingInviteUser(@Param('id') id: string) {
+    const students = await this.inviteService.getPendingInviteUser(id);
+    return students;
   }
 }
--- apps/backend/src/invite/invite.service.ts
+++ apps/backend/src/invite/invite.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateClassInviteDto } from './dto/create-invite.dto';
 import { PrismaService } from '../prisma/prisma.service';
-import { log } from 'console';
 
 @Injectable()
 export class InviteService {
@@ -14,8 +13,6 @@ export class InviteService {
     if (!user) {
       throw new NotFoundException();
     }
-
-    console.log(createInviteDto)
 
     const student = await this.prismaService.student.findUnique({
       where: { userId: user.id },
@@ -34,20 +31,28 @@ export class InviteService {
   }
 
   async acceptById(id: string) {
-    console.log(id);
     const invitationToClass =
       await this.prismaService.invitationToClass.findUnique({ where: { id } });
     if (!invitationToClass) {
       throw new NotFoundException();
     }
-    console.log(invitationToClass);
-  
+
     const updatedStudent = await this.prismaService.student.update({
       where: { id: invitationToClass.studentId },
       data: { class: { connect: { id: invitationToClass.classId } } },
     });
-  
+
     await this.prismaService.invitationToClass.delete({ where: { id: id } });
     return updatedStudent;
+  }
+
+  async getPendingInviteUser(classId: string) {
+    const invitationToClass =
+      await this.prismaService.invitationToClass.findMany({
+        where: {classId: classId},
+        include: { student: { include: { user: true } } },
+      });
+
+    return invitationToClass.map((invitation) => invitation.student);
   }
 }
